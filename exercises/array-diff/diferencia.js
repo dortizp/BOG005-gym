19f58d527baa236f2eb49e953681a901cfd40614
David Ortiz <david.ortiz.p@gmail.com>
2022-11-17 00:40:17 -0500

    first attempt for diferencia function

--- exercises/array-diff/diferencia.js
+++ exercises/array-diff/diferencia.js
@@ -1,3 +1,7 @@
-function diferencia() {
-    console.log("funcion diferencia")
+function diferencia(arr1, arr2) {
+    return arr1.filter(x => x !== arr2[0])
 }
+
+const a = [1,2,3]
+const b = [2]
+console.log(diferencia(a,b))
